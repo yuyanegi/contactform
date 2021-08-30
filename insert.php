96d782ebbaa66b3983f26a561dde363e4f8317de
Your Name <you@example.com>
2021-08-30 10:31:44 +0900

    s最初のコミット

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php
+mb_internal_encoding("utf8");
+$pdo = new PDO("mysql:dbname=lolo;host=localhost;","root","");
+
+$pdo->exec("insert into contactfrom(name,mail,age,comments)
+values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');");
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<meta charset="utf-8">
+<head>
+<title>お問い合わせフォームを作る</title>
+ <link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+
+    <body>
+        <h1>お問い合わせフォーム</h1>
+        <div class="confirm">
+            <p>お問合せ有難うございました。<br>3営業以内に担当者よりご連絡差し上げます。</p>
+        </div>
+    </body>
+</html>
